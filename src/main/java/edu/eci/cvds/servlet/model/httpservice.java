dcf6c28a231e8f86ee7ac1f9d01009fae976c5c2
daniel gomez <daniel.gomez-su@linux-6.labinfo.is.escuelaing.edu.co>
2020-02-27 12:11:46 -0500

    parte II final

--- src/main/java/edu/eci/cvds/servlet/model/httpservice.java
+++ src/main/java/edu/eci/cvds/servlet/model/httpservice.java
@@ -7,8 +7,10 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.Writer;
+import java.net.MalformedURLException;
 import java.util.ArrayList;
 import java.util.Optional;
 
@@ -21,33 +23,52 @@ public class httpservice extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        Writer responseWriter = resp.getWriter();
         try {
-            Writer responseWriter = resp.getWriter();
+
             Optional<Integer> optId = Optional.ofNullable(Integer.parseInt(req.getParameter("id")));
             Integer id = optId.isPresent() ? optId.get():1;
             todosList.add(Service.getTodo(id));
             resp.setStatus(HttpServletResponse.SC_OK);
             responseWriter.write(Service.todosToHTMLTable(todosList));
             responseWriter.flush();
-        }catch (IOException e){
+        }catch ( NumberFormatException e){
+            responseWriter.write("Requerimiento Inválido");
             resp.setStatus(HttpServletResponse.SC_NOT_FOUND);
-        }
-
+        }catch (FileNotFoundException e){
+            resp.setStatus(HttpServletResponse.SC_NOT_FOUND);
+            responseWriter.write("\n");
+            responseWriter.write("No encontrado.");
+        }catch (MalformedURLException e){
+            responseWriter.write("Error interno en el Servidor ");
+        }catch (Exception e){
+        responseWriter.write("requerimiento inválido");
+    }
     }
 
     @Override
     protected void doPost (HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        Writer responseWriter = resp.getWriter();
         try {
-            Writer responseWriter = resp.getWriter();
+
             Optional<Integer> optId = Optional.ofNullable(Integer.parseInt(req.getParameter("id")));
             Integer id = optId.isPresent() ? optId.get():1;
             todosList.add(Service.getTodo(id));
             resp.setStatus(HttpServletResponse.SC_OK);
             responseWriter.write(Service.todosToHTMLTable(todosList));
             responseWriter.flush();
-        }catch (IOException e){
+        }catch ( NumberFormatException e){
+            responseWriter.write("requerimiento inválido ");
             resp.setStatus(HttpServletResponse.SC_NOT_FOUND);
-        }
+        }catch (FileNotFoundException e){
+            resp.setStatus(HttpServletResponse.SC_NOT_FOUND);
+            responseWriter.write("\n");
+            responseWriter.write("fuera de rango.");
+        }catch (MalformedURLException e){
+            responseWriter.write("Error interno en el Servidor ");
 
+        }catch (Exception e){
+            responseWriter.write("requerimiento inválido");
+        }
     }
 }
